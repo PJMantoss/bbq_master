7920e6f0a9cbdb7a702514ebf08945d00f447b4b
pjmantoss <joelptoss@gmail.com>
2020-10-10 09:38:52 +0100

    update script

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 let emailCollector = document.getElementById('emailCollector');
 
-emailCollector.addEventListener('submit', () => {})
+emailCollector.addEventListener('submit', e => {
+    //Stop default event behaviour
+    e.preventDefault();
+
+    //Use FormData to get user's name and email
+    let ourFormData = new FormData(e.target);
+})
